16293ce2d264a3ac66f167f985ae30a67546f03c
Keegan Jordan <keeganrjordan@gmail.com>
2024-11-21 07:13:35 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Check robinhood and DB hourly. 
+# Compare robinhood to DB and updates DB. 
+# Apply algorithm to each stock in DB. 
+def main():
+  print("main")
+
+# Get array of stocks from RH and array of stocks from DB.
+# Compare arrays and update DB if there are differences.
+def compareRHToDB():
+  print("compareRHToDB")
+
+# Get double array of stocks with their information.
+def getRHList():
+  print("chgetRHListeckRH")
+
+# Get double array of stocks with their information.
+def getDBList():
+  print("getDBList")
+
+# Loop through DB and apply decidePosition to each stock.
+def algo():
+  print("algo")
+
+# Decide if stock should be held, bought or sold. 
+def decidePosition():
+  print("decidePosition")
